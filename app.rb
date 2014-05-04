70d9761fc51c5eb5aafde5d99d4bf327075ea213
Casey Kolderup <casey@kolderup.org>
2014-05-04 13:06:23 -0400

    add irondavy's occupations list

--- app.rb
+++ app.rb
@@ -39,8 +39,12 @@ end
 
 
 red_sky = nil
-while red_sky.nil? do
-    sailor = File.readlines('./ors.txt').sample.chomp
+length = 141
+while red_sky.nil? || length > 140 do
+    sailor_source = Array.new(9, "./jobs.txt") +
+                          Array.new(1, "./ors.txt")
+
+    sailor = File.readlines(sailor_source.sample).sample.chomp
     red_sky = client.search("\"#{SEARCH_QUERY}\"", result_type: "recent").
         collect.take(500).flat_map { |tweet|
             tweet.text.match(/#{SEARCH_QUERY}([a-zA-Z0-9 &\-']+)/i).andand.captures.andand.first
@@ -51,9 +55,10 @@ while red_sky.nil? do
             Obscenity.profane?(text) ||
             has_blacklisted(text)
         }.sample.strip
+        output = "#{red_sky} at morning, #{sailor}s take warning. #{red_sky} at night, #{sailor}s' delight"
+        length = output.length
 end
 
-output = "#{red_sky} at morning, #{sailor}s take warning. #{red_sky} at night, #{sailor}s' delight"
 
 if options[:tweet] then
     client.update output
